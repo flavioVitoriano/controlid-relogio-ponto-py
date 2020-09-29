581d3880d44473e507b58d687818e7e727af3b9b
fvitoriano <flavio.vitorianodev@gmail.com>
2020-09-29 08:23:51 -0300

    udpated example script

--- example.py
+++ example.py
@@ -1,11 +1,19 @@
+#!/usr/bin/python3
 from controlidpy.entities import Session
 from controlidpy.functions import get_afds, get_users_from_afds, transform_afds, to_csv
 
 
 session = Session("admin", "admin", "https://192.168.0.98")
-afds = get_afds(session, 10, 4, 2020)
+
+day = int(input("digite o dia: "))
+month = int(input("digite o mes: "))
+year = int(input("digite o ano: "))
+
+afds = get_afds(session, day, month, year)
 users = get_users_from_afds(session, afds)
 final = transform_afds(afds, users)
 
-to_csv(final, "csv_example.csv")
+print(f"importados {len(final)} afds!")
+name = input("digite o nome do arquivo para salvar: ")
 
+to_csv(final, f"{name}.csv")
